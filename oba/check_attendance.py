292b1693908d5f831803cd8ab57b0f3cd3a034cc
philip-baker <65636251+philip-baker@users.noreply.github.com>
2020-11-03 14:01:22 +1300

    Update check_attendance.py commenting

--- oba/check_attendance.py
+++ oba/check_attendance.py
@@ -21,29 +21,26 @@ sys.path.append(os.getcwd() +'/models/model-r100-ii')
 os.chdir(currDir)
 import argparse
 import cv2
-import sys
 import numpy as np
-import os
 import json
 from csv import writer
 from datetime import datetime
 import sqlite3
-import cv2
-import argparse
 from io import BytesIO
 import torch
 import PIL.Image
 from torchvision import transforms
-import face_model
-from engagement_model import EngageModel
-from functions import get_detections
-from model.utils import get_model
 
 
 __author__ = "Philip Baker & Keith Spencer-Edgar"
 __date__ = "25-10-2020"
 
-# user arguments
+import face_model
+from engagement_model import EngageModel
+from functions import get_detections
+from model.utils import get_model
+
+# User arguments
 parser = argparse.ArgumentParser(description='face model test')
 parser.add_argument('--image-size', default='112,112', help='')
 parser.add_argument('--model', default='../models/model-r100-ii/model,0', help='path to load model.')
@@ -55,12 +52,12 @@ parser.add_argument('--code', default='my_course', type=str, help='The course co
 args = parser.parse_args()
 
 
-# use open csv to take an image of the audience
-
+# Check for a user supplied image 
 count = 0
 for filename in os.listdir('sample_images'):
     if filename.endswith(".jpg"):
         count = count + 1
+ # Use open cv to take an image of the audience
 if count == 0:
     cap = cv2.VideoCapture(0) # video capture source camera (Here webcam of laptop)
     ret, frame = cap.read() # return a single frame in variable `frame`
@@ -74,7 +71,7 @@ if count == 0:
 
 class_image =  'sample_images/sample.jpg'
 
-# run tiny faces
+# Arguments for the Tiny Face Detector
 class args_eval():
     def __init__(self):
         self.nms_thresh = 0.3
@@ -86,37 +83,37 @@ class args_eval():
 
 args_tinyface = args_eval()
 
-# get templates for Tiny Face Detector
+# Get templates for Tiny Face Detector
 templates = json.load(open(args_tinyface.template_file))
 json.dump(templates, open(args_tinyface.template_file, "w"))
 templates = np.round_(np.array(templates), decimals=8)
 num_templates = templates.shape[0]
 
-# getting transforms - probably needed
+# Getting transforms for the Tiny Face Detector
 normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 val_transforms = transforms.Compose([transforms.ToTensor(), normalize])
 
-
+# Specify whether to run on GPU or CPU based on user arguments 
 if args.gpu > 0:
     device = torch.device('gpu')
 else:
     device = torch.device('cpu')
 
-# load the Tiny Face Detector
+# Load the Tiny Face Detector
 model_tinyfaces = get_model(args_tinyface.checkpoint, num_templates=num_templates, gpu= (args.gpu > 0))
 
-# load the insightface ArcFace Face Recognition model
-# from model tinyfaces utils
-model_arcface = face_model.FaceModel(args)
-
+# Specifications for the Tiny Face Detector
 rf = {
     'size': [859, 859],
     'stride': [8, 8],
     'offset': [-1, -1]
 }
 
+# Load the insightface ArcFace Face Recognition model
+model_arcface = face_model.FaceModel(args)
 
-# find detections with tinyface on downscaled photo
+
+# Downscale iamge 
 img = PIL.Image.open(class_image)
 basewidth = 800
 scale_ = (basewidth / float(img.size[0]))
@@ -126,7 +123,7 @@ if float(img.size[0]) > basewidth:
 else:
     scale_ = 1
     img_downscaled = img.convert('RGB')
-
+# User Tiny Face Detector to get detections from downscaled image 
 img_tensor = transforms.functional.to_tensor(img_downscaled)
 dets = get_detections(model_tinyfaces, img_tensor, templates, rf, val_transforms,
                       prob_thresh=args_tinyface.prob_thresh,
@@ -136,27 +133,27 @@ img = PIL.Image.open(class_image)
 class_faces = list()
 class_scores = list()
 
-# resize image according to quality
+# Resize image according to quality
 basewidth = int(float(img.size[0]))
 hsize = int(float(img.size[1]))
 this_img = img.resize((basewidth, hsize), PIL.Image.ANTIALIAS).convert('RGB')
 
-# load student profile embeddings for the class in progress
+# Load student profile embeddings for the class in progress
 data = EngageModel.get_profiles(args)
 
-# get all detections for this image quality
-for i in range(len(dets)):  # for each detection
-    if dets[i][4] > args_tinyface.threshold_score:  # if the tinyfaces score is good
+# Get all detections
+for i in range(len(dets)):  
+    if dets[i][4] > args_tinyface.threshold_score:  # Check detector confidence is above threshold
         bbox = dets[i][0:4]
         new_bbox = bbox * (1 / scale_)
         face_width = new_bbox[2] - new_bbox[0]
-        this_face = np.array(this_img.crop(new_bbox))  # get cropped face
-        class_faces.append(list([this_face[:, :, ::-1].copy(), face_width]))  # append the cropped face
+        this_face = np.array(this_img.crop(new_bbox))  # Get cropped face
+        class_faces.append(list([this_face[:, :, ::-1].copy(), face_width]))  # Append the cropped face
 
-# calculate arcface embeddings for each sample picture
+# Calculate arcface embeddings for each sample picture
 detection_features, face_widths = EngageModel.get_embeddings(model_arcface, class_faces)
 
-# compare samples to profile face embeddings
+# Compare samples to profile face embeddings
 engage_model = EngageModel(args)
 EngageModel.compare_embeddings(engage_model, detection_features, data)
 
